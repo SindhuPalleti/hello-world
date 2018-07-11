06783c8900c69def868e5caa9b07fd3f2dfcd6b5
SindhuPalleti <sindhureddypalleti@gmail.com>
2018-07-11 22:21:49 +0530

    variables prog

---
+++ variables.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+var c,python bool
+var j int
+var k,l int = 1,2
+
+func main() {
+	var i int
+	var java,cplus,z = true, false, "no!"
+	m,n:=3,4
+	fmt.Println(c,python,j)
+	fmt.Println(k,l)
+	fmt.Println(java,cplus,z,i,m,n)
+}
